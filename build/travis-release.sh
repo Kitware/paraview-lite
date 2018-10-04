2a44e34f9db87f7cb151c7dbe124a87f23ebcc36
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-10-03 18:12:26 -0600

    ci(travis): Update config

--- build/travis-release.sh
+++ build/travis-release.sh
@@ -5,12 +5,12 @@ set -ev
 NIGHTLY_VERSION=`npm info | grep latest | cut -d " " -f 2`
 echo "window.PARAVIEW_LITE_VERSION = '$NIGHTLY_VERSION';" > dist/version.js
 
-npm run build:release
-npm run validate
-git config --global user.name "Travis CI"
-git config --global user.email "sebastien.jourdain@kitware.com"
-export GIT_PUBLISH_URL=https://${GH_TOKEN}@github.com/Kitware/light-viz.git
-npm run semantic-release
+# npm run build:release
+# npm run validate
+# git config --global user.name "Travis CI"
+# git config --global user.email "sebastien.jourdain@kitware.com"
+# export GIT_PUBLISH_URL=https://${GH_TOKEN}@github.com/Kitware/paraview-lite.git
+# npm run semantic-release
 
 # Needed if not a release for nightly bundle
 npm run bundle
