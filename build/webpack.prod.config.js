499c2036f5eb9328666aecc6b13f3c98b4960585
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-10-05 13:59:17 -0600

    fix: 🐛 Update config for new build tools

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,4 +1,3 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 
@@ -9,20 +8,6 @@ const PORT = process.env.PORT && Number(process.env.PORT);
 
 module.exports = merge(baseConfig, {
   mode: 'production',
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          compress: {
-            // uglifyjs bug: https://github.com/webpack/webpack/issues/6567
-            inline: false,
-          },
-        },
-      }),
-    ],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
