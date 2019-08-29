d2dfaf23842314e2f54de8baaa4180ff270de095
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2019-08-29 15:59:58 -0600

    chore(linter): enable console log

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/prettier'],
   rules: {
     'import/extensions': 0,
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
