f7d58ab125712accdeb50d3505cd7d29c142a53d
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2019-08-29 15:52:20 -0600

    ci(travis): Update bundle script

--- build/travis-release.sh
+++ build/travis-release.sh
@@ -8,15 +8,12 @@ mkdir -p ./dist
 NIGHTLY_VERSION=`npm info | grep latest | cut -d " " -f 2`
 echo "window.PARAVIEW_LITE_VERSION = '$NIGHTLY_VERSION';" > dist/version.js
 
-npm run build:release
-npm run validate
+npm run build
+npm run lint
 git config --global user.name "Travis CI"
 git config --global user.email "sebastien.jourdain@kitware.com"
 export GIT_PUBLISH_URL=https://${GH_TOKEN}@github.com/Kitware/paraview-lite.git
 npm run semantic-release
-
-# Needed if not a release for nightly bundle
-npm run bundle
 
 # Generate website
 npm run doc:publish
