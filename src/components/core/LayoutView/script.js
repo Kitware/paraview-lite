4c5c6c5334f9574cc86a556e18f04f18e7ff5488
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-08-20 08:56:19 -0600

    fix(view): Connect settings to view

--- src/components/core/LayoutView/script.js
+++ src/components/core/LayoutView/script.js
@@ -15,10 +15,17 @@ export default {
     ...mapGetters({
       wsClient: 'PVL_NETWORK_CLIENT',
       viewId: 'PVL_VIEW_ID',
+      quality: 'PVL_VIEW_QUALITY_INTERACTIVE',
+      ratio: 'PVL_VIEW_RATIO_INTERACTIVE',
     }),
     smallScreen() {
       return this.$vuetify.breakpoint.width < Breakpoints.md;
     },
+  },
+  watch: {
+    quality(q) {
+      this.$refs.remoteView.setInteractiveQuality(q);
+    }
   },
   components: {
     RepresentationToolbar,
